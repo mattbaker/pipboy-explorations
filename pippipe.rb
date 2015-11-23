7142ab589f8b08798c0409cb33b3544ac8e7122e
Matt Baker <mbaker.pdx@gmail.com>
2015-11-22 22:11:11 -0600

    use Message in pippipe

--- pippipe.rb
+++ pippipe.rb
@@ -1,12 +1,13 @@
 require 'socket'
+require_relative 'pipboy/message'
 
 STDOUT.sync = true
 
 socket = TCPSocket.new('192.168.0.101', 27000)
 
 while line = socket.recv(8)
-  if line == "\x00\x00\x00\x00\x00"
-    socket.send("\x00\x00\x00\x00\x00", 0)
+  if line == Pipboy::Message.keep_alive.to_bytes
+    socket.send(Pipboy::Message.keep_alive.to_bytes, 0)
   end
   n = STDOUT.write(line)
 end
