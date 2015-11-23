cb9e2c5f9b7f2863462e69e84464369db02bc337
Matt Baker <mbaker.pdx@gmail.com>
2015-11-22 22:45:55 -0600

    improve formatting

--- pipparse.rb
+++ pipparse.rb
@@ -2,10 +2,14 @@ require_relative 'pipboy/message'
 
 STDOUT.sync = true
 STDIN.sync = true
+MAX_PAYLOAD_LENGTH = 64
 
 i = 0
 until STDIN.eof? do
   message = Pipboy::Message.from_stream(STDIN)
   puts "#{i.to_s.rjust(5, "0")}: #{message.type} with length #{message.length}"
+  unless message.body.empty?
+    puts "       #{message.hex_body[0...MAX_PAYLOAD_LENGTH]}#{"..." if message.hex_body.length > MAX_PAYLOAD_LENGTH}"
+  end
   i+=1
 end
