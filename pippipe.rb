0ed12bd687d091b95045addff16eaeac93fd3129
Matt Baker <mbaker.pdx@gmail.com>
2015-11-22 22:12:38 -0600

    assign to variable upfront

--- pippipe.rb
+++ pippipe.rb
@@ -4,11 +4,13 @@ require_relative 'pipboy/message'
 STDOUT.sync = true
 
 socket = TCPSocket.new('192.168.0.101', 27000)
+keep_alive_msg = Pipboy::Message.keep_alive.to_bytes
 
 while line = socket.recv(8)
-  if line == Pipboy::Message.keep_alive.to_bytes
-    socket.send(Pipboy::Message.keep_alive.to_bytes, 0)
+  if line == keep_alive_msg
+    socket.send(keep_alive_msg, 0)
   end
   n = STDOUT.write(line)
 end
+
 socket.close
