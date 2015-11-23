388a99f341848656a80782184850848700413808
Matt Baker <mbaker.pdx@gmail.com>
2015-11-22 22:28:54 -0600

    tweak output

--- pipparse.rb
+++ pipparse.rb
@@ -3,7 +3,9 @@ require_relative 'pipboy/message'
 STDOUT.sync = true
 STDIN.sync = true
 
+i = 0
 until STDIN.eof? do
   message = Pipboy::Message.from_stream(STDIN)
-  puts "Received #{message.type} with length #{message.length}"
+  puts "#{i.to_s.rjust(5, "0")}: #{message.type} with length #{message.length}"
+  i+=1
 end
