ee6b6c70e6c7318663de06bfccc20422461a8cb2
Matt Baker <mbaker.pdx@gmail.com>
2015-11-27 11:11:42 -0800

    skeleton for composite readers test

---
+++ test/pipboy/parser/composite_readers_test.rb
@@ -0,0 +1,20 @@
+require_relative "../../../pipboy/parser/composite_readers"
+require "minitest/autorun"
+require 'stringio'
+
+class TestPipboyCompositeReaders < Minitest::Test
+  def setup
+  end
+
+  def test_header
+  end
+
+  def test_list
+  end
+
+  def test_dict_entry
+  end
+
+  def test_dict_update
+  end
+end
