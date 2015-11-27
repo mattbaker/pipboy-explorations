1a9caa0fb78897702f83358c145b3fdfb77b4501
Matt Baker <mbaker.pdx@gmail.com>
2015-11-27 11:19:03 -0800

    Move data_update into message readers

--- pipboy/parser/message_readers.rb
+++ pipboy/parser/message_readers.rb
@@ -11,6 +11,30 @@ module Pipboy
         "SERVER BUSY"
       end
 
+      def read_data_update(stream, length)
+        stop_position = stream.pos+length
+        updates = []
+        while stream.pos < stop_position
+          type  = PrimitiveReaders.uint8(stream)
+          id    = PrimitiveReaders.reference(stream)
+          value = case type
+          when 0 then PrimitiveReaders.bool(stream)
+          when 1 then PrimitiveReaders.sint8(stream)
+          when 2 then PrimitiveReaders.uint8(stream)
+          when 3 then PrimitiveReaders.sint32(stream)
+          when 4 then PrimitiveReaders.uint32(stream)
+          when 5 then PrimitiveReaders.float32(stream)
+          when 6 then PrimitiveReaders.string(stream)
+          when 7 then CompositeReaders.list(stream, PrimitiveReaders, :reference)
+          when 8 then CompositeReaders.dict_update(stream)
+          else
+            raise "Unknown Data Type #{type}"
+          end
+          updates << {set: id, value: value}
+        end
+        updates
+      end
+
       def keep_alive(stream)
         "KEEP ALIVE"
       end
--- pipboy/parser/parser.rb
+++ pipboy/parser/parser.rb
@@ -22,36 +22,12 @@ module Pipboy
       when 0 then MessageReaders.keep_alive(stream)
       when 1 then MessageReaders.connection_accepted(stream, length)
       when 2 then MessageReaders.connect_refused(stream)
-      when 3 then read_data_update(stream, length)
+      when 3 then MessageReaders.data_update(stream, length)
       else
         "NOT IMPLEMENTED"
       end
 
       {message_type: OPCODE_NAMES[opcode], message_body: body}
-    end
-
-    def read_data_update(stream, length)
-      stop_position = stream.pos+length
-      updates = []
-      while stream.pos < stop_position
-        type = PrimitiveReaders.uint8(stream)
-        id = PrimitiveReaders.reference(stream)
-        value = case type
-                when 0 then PrimitiveReaders.bool(stream)
-                when 1 then PrimitiveReaders.sint8(stream)
-                when 2 then PrimitiveReaders.uint8(stream)
-                when 3 then PrimitiveReaders.sint32(stream)
-                when 4 then PrimitiveReaders.uint32(stream)
-                when 5 then PrimitiveReaders.float32(stream)
-                when 6 then PrimitiveReaders.string(stream)
-                when 7 then CompositeReaders.list(stream, PrimitiveReaders, :reference)
-                when 8 then CompositeReaders.dict_update(stream)
-                else
-                  raise "Unknown Data Type #{type}"
-                end
-        updates << {set: id, value: value}
-      end
-      updates
     end
   end
 end
