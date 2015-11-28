be1e903f02dee3e960ce92d5941e54e1a545187f
Bontgoy <tyjkeating@gmail.com>
2015-11-28 01:44:39 -0600

    composite_readers_tests test_header and test_list passing

--- test/pipboy/parser/composite_readers_test.rb
+++ test/pipboy/parser/composite_readers_test.rb
@@ -1,3 +1,4 @@
+require_relative "../../../pipboy/parser/primitive_readers"
 require_relative "../../../pipboy/parser/composite_readers"
 require "minitest/autorun"
 require 'stringio'
@@ -7,9 +8,14 @@ class TestPipboyCompositeReaders < Minitest::Test
   end
 
   def test_header
+    headerBytes = StringIO.new("\xA0\x00\x00\x00\x03")
+    assert_equal([160,3], Pipboy::Parser::CompositeReaders.header(headerBytes))
   end
 
   def test_list
+    primitiveReaders = Pipboy::Parser::PrimitiveReaders 
+    streamBytes = StringIO.new("\x03\x00\x00\x00\x03\x48\x45\x4C\x4C\x4F\x57\x4F\x52\x4C")
+    assert_equal([1208156160, 1330400325, 1280462679], Pipboy::Parser::CompositeReaders.list(streamBytes, primitiveReaders, :uint32))
   end
 
   def test_dict_entry
