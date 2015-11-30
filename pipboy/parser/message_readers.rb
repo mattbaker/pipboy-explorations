3405055c7b6cf4a76f3ed3296aab0b2de94d06e8
Matt Baker <mbaker.pdx@gmail.com>
2015-11-29 22:13:18 -0600

    first pass at a DB that represents all this game data

--- pipboy/parser/message_readers.rb
+++ pipboy/parser/message_readers.rb
@@ -1,6 +1,7 @@
 require 'json'
 require_relative 'primitive_readers'
 require_relative 'composite_readers'
+require_relative '../db/updates'
 
 module Pipboy
   module Parser
@@ -17,26 +18,29 @@ module Pipboy
 
       def data_update(stream, length)
         stop_position = stream.pos+length
-        updates = []
+        database_updates = []
+
         while stream.pos < stop_position
-          type  = PrimitiveReaders.uint8(stream)
-          id    = PrimitiveReaders.reference(stream)
-          value = case type
-          when 0 then PrimitiveReaders.bool(stream)
-          when 1 then PrimitiveReaders.sint8(stream)
-          when 2 then PrimitiveReaders.uint8(stream)
-          when 3 then PrimitiveReaders.sint32(stream)
-          when 4 then PrimitiveReaders.uint32(stream)
-          when 5 then PrimitiveReaders.float32(stream)
-          when 6 then PrimitiveReaders.string(stream)
-          when 7 then CompositeReaders.list(stream, PrimitiveReaders, :reference)
-          when 8 then CompositeReaders.dict_update(stream)
+          type   = PrimitiveReaders.uint8(stream)
+          id     = PrimitiveReaders.reference(stream)
+          update = case type
+          when 0 then DB::Updates::Value.new(id, PrimitiveReaders.bool(stream))
+          when 1 then DB::Updates::Value.new(id, PrimitiveReaders.sint8(stream))
+          when 2 then DB::Updates::Value.new(id, PrimitiveReaders.uint8(stream))
+          when 3 then DB::Updates::Value.new(id, PrimitiveReaders.sint32(stream))
+          when 4 then DB::Updates::Value.new(id, PrimitiveReaders.uint32(stream))
+          when 5 then DB::Updates::Value.new(id, PrimitiveReaders.float32(stream))
+          when 6 then DB::Updates::Value.new(id, PrimitiveReaders.string(stream))
+          when 7 then DB::Updates::Value.new(id, CompositeReaders.list(stream, PrimitiveReaders, :reference))
+          when 8 then DB::Updates::Dictionary.new(id, *CompositeReaders.dict_update(stream))
           else
             raise "Unknown Data Type #{type}"
           end
-          updates << {set: id, value: value}
+
+          database_updates << update
         end
-        updates
+
+        database_updates
       end
 
       def keep_alive(stream)
--- pipparse.rb
+++ pipparse.rb
@@ -1,12 +1,11 @@
 require 'pp'
 require 'json'
 require_relative 'pipboy/parser/parser'
+require_relative 'pipboy/db/database'
 
 STDOUT.sync = true
 STDIN.sync = true
-MAX_PAYLOAD_LENGTH = 64
 
 until STDIN.eof? do
-  message = Pipboy::Parser.parse_message(STDIN)
-  STDOUT.puts(message.to_json)
+  STDOUT.puts(Pipboy::Parser.parse_message(STDIN).to_json)
 end
