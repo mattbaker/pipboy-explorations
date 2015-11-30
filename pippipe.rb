5c778e162b0c3d2398e97e8c19e0e49e1d1dd9df
Bontgoy <tyjkeating@gmail.com>
2015-11-29 19:04:13 -0600

    correct composite reader test dict update

--- pippipe.rb
+++ pippipe.rb
@@ -5,7 +5,6 @@ STDOUT.sync = true
 ip = ARGV[0]
 socket = TCPSocket.new(ip, 27000)
 keep_alive_msg = Pipboy::Parser::MessageReaders.keep_alive(socket)
-
 while line = socket.recv(8)
   if line == keep_alive_msg
     socket.send(keep_alive_msg, 0)
